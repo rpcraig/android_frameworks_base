b8d01ca317bdf7a2cd798747011beded081f8495
Stephen Smalley <sds@tycho.nsa.gov>
2012-03-19 10:41:00 -0400

    Added SELinux enforcing status and booleans keys to secure system settings.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4082,6 +4082,25 @@ public final class Settings {
                 "contacts_preauth_uri_expiration";
 
         /**
+         * SELinux enforcing status.
+         * 1 - SELinux is in enforcing mode.
+         * 0 - SELinux is in permissive mode.
+         *
+         * @hide
+         */
+        public static final String SELINUX_ENFORCING = "selinux_enforcing";
+
+        /**
+         * Stores the values of the SELinux booleans. Stored as a comma
+         * seperated list of values, each value being of the form
+         * {@code boolean_name:value} where value is 1 if the boolean is set
+         * and 0 otherwise. Example: {@code bool1:1,bool2:0}.
+         *
+         * @hide
+         */
+        public static final String SELINUX_BOOLEANS = "selinux_booleans";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -4119,7 +4138,9 @@ public final class Settings {
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
             LOCK_SCREEN_OWNER_INFO,
-            LOCK_SCREEN_OWNER_INFO_ENABLED
+            LOCK_SCREEN_OWNER_INFO_ENABLED,
+            SELINUX_ENFORCING,
+            SELINUX_BOOLEANS
         };
 
         /**
